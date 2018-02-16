3dee5a9b7f7b7073921ef9819b3cb4ee20fe1519
JordySL <jordyfaren@gmail.com>
2018-02-16 01:13:31 -0500

    Agregando Servicios al Producto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
-
+import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    SharedModule, AppRoutingModule
+    SharedModule, AppRoutingModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/producto/producto.module.ts
+++ src/app/producto/producto.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ProductoListaComponent } from './producto-lista/producto-lista.component';
 import { ProductoRoutingModule } from './producto-routing.module';
+import { ProductoService } from './producto.service';
 @NgModule({
   imports: [
     CommonModule,
     ProductoRoutingModule
   ],
-  declarations: [ProductoListaComponent]
+  declarations: [ProductoListaComponent],
+  providers: [ProductoService]
 })
 export class ProductoModule { }
